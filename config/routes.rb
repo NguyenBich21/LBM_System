54b7cf7475dd3b26b98e4891292f37d595f1e654
NguyenBich21 <vantang200378@gmail.com.vn>
2023-12-28 04:10:24 +0700

    Add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   # Defines the root path route ("/")
   root "home#index"
